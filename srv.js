463c3c9ca84ba79819e7b768f1976c437328f694
roman.alecseevich <romabk@bk.ru>
2018-05-17 17:27:16 +0300

    first2

--- srv.js
+++ srv.js
@@ -1,7 +1,7 @@
 const http = require('http');
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-http.createServer(function (request, response) {
+const server = http.createServer(function (request, response) {
    // Send the HTTP header
    // HTTP Status: 200 : OK
    // Content Type: text/plain
@@ -9,7 +9,7 @@ http.createServer(function (request, response) {
 
    // Send the response body as "Hello World"
    response.end('Hello World\n');
-}).listen(server_port);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+});
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
