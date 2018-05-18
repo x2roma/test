be15c825627ec5be7a656ba83862c2a47d7a8f59
roman.alecseevich <romabk@bk.ru>
2018-05-18 10:44:55 +0300

    first4

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+const server = http.createServer(function (request, response) {
+   // Send the HTTP header
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+});
+server.listen(port, ip, function () {
+  console.log( "Listening on " + ip + ", port " + port )
+});
